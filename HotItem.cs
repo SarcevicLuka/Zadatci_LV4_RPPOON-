a61d673eae740f137d6ac00309c96dd62fd53cab
Luka Šarčević <5227796-luka_sarcevic@users.noreply.gitlab.com>
2020-04-30 17:54:03 +0000

    Add new file

---
+++ HotItem.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatci_3_5
+{
+    class HotItem:RentableDecorator
+    {
+        private readonly double HotItemBonus = 1.99;
+        public HotItem(IRentable rentable) : base(rentable) { }
+        public override double CalculatePrice()
+        {
+            return base.CalculatePrice() + this.HotItemBonus;
+        }
+        public override String Description
+        {
+            get
+            {
+                return "Trending: " + base.Description;
+            }
+        }
+    }
+}
